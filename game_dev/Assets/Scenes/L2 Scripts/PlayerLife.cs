e3c2d7c2b24d0d352c0fda216d4ab552ba6b336f
saiaakaash <86718211+saiaakaash@users.noreply.github.com>
2023-11-23 23:18:33 +0530

    L2 outro added

---
+++ game_dev/Assets/Scenes/L2 Scripts/PlayerLife.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class player : MonoBehaviour
+{
+   private void onCollisionEnter(Collision collision)
+   {
+   if (collision.gameObject.CompareTag("server"))
+   {
+   Die();
+   }
+   if (collision.gameObject.CompareTag("screen"))
+   {
+   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)
+   }
+   }
+   void Die()
+   {
+   GetComponent<MeshRenderer>().enabled = false;
+   GetComponent<Rigidbody>().isKinematic = true;
+   GetComponent<PlayerMovement>().enabled = false;
+   Invoke(nameof (ReloadLevel), 1.3f);
+   }
+   void ReloadLevel()  
+   {
+   SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+   }
+}
