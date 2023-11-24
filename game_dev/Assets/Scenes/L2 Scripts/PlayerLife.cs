d30318f2c2dc9eab06223281040bc9720e7eae35
saiaakaash <86718211+saiaakaash@users.noreply.github.com>
2023-11-24 07:32:17 +0530

    finished L2

--- game_dev/Assets/Scenes/L2 Scripts/PlayerLife.cs
+++ game_dev/Assets/Scenes/L2 Scripts/PlayerLife.cs
@@ -1,26 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
-   private void onCollisionEnter(Collision collision)
-   {
+   private Canvas deathCanvas;
+    private bool isDead = false;
+/*
+    private void OnCollisionEnter(Collision collision)
+    {
+         Debug.Log("DED");
+        if (collision.gameObject.tag == "server")
+        {
+         Debug.Log("double DED");
+            Destroy(gameObject);
+        }
+    }*/
+
+
+   
+   private void Start()
+    {
+      Cursor.lockState = CursorLockMode.Locked;
+        deathCanvas =  GameObject.FindWithTag("deathnote").GetComponent<Canvas>();
+        deathCanvas.gameObject.SetActive(false);
+    }
+   public void OnCollisionEnter(Collision collision)
+   {Debug.Log("COllition");
    if (collision.gameObject.CompareTag("server"))
    {
-   Die();
+      Debug.Log("DEAD");
+      Die();
+      // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+   
    if (collision.gameObject.CompareTag("screen"))
    {
-   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)
+      Debug.Log("COMPLETE");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+   if (collision.gameObject.CompareTag("Enemy"))
+   {
+      Debug.Log("DEAD");
+      Die();
+   }
+
    }
    void Die()
    {
-   GetComponent<MeshRenderer>().enabled = false;
-   GetComponent<Rigidbody>().isKinematic = true;
-   GetComponent<PlayerMovement>().enabled = false;
+   //GetComponent<MeshRenderer>().enabled = false;
+   //GetComponent<Rigidbody>().isKinematic = true;
+   //GetComponent<PlayerMovement>().enabled = false;
+   isDead = true;
+   deathCanvas.gameObject.SetActive(true);
    Invoke(nameof (ReloadLevel), 1.3f);
    }
    void ReloadLevel()  
